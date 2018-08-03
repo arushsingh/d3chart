047493948b45f431b88cca2277f6440162a431ae
arush@snakebite.com <arush@snakebite.com>
2018-08-03 23:36:55 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Centertree from './centerTree';
+import './App.css';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Centertree />
+      </div>
+    );
+  }
+}
+
+export default App;
